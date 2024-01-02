1f9b3e1c6f4cb6294e60c2ee2e750461b5a74d53
majster247 <htopolski12@gmail.com>
2024-01-02 10:58:45 +0100

    start of project

---
+++ mentaltracker/src/components/publicMaps.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function publicMap(){
+}
+
+export default publicMap;
---
+++ mentaltracker/src/components/publicTree.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Baobab(){
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
