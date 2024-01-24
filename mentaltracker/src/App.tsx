20d48ea9cc2c84fb13470592a6c2f7ac399b4f9d
Majster247 <htopolski12@gmail.com>
2024-01-24 10:30:42 +0100

    baobab

--- mentaltracker/src/App.tsx
+++ mentaltracker/src/App.tsx
@@ -26,7 +26,7 @@ const App: React.FC = () => {
           <Route path="/goal/:id" element={<GoalDetails />} />
           <Route path="/home" element={<MainPage />} />
           <Route path='' element={<MainPage />} />
-          <Route path='/MentalTracker' element={<MainPage />} />
+          <Route path='/MentalTracker/' element={<MainPage />} />
 
           <Route
             path="*"
--- mentaltracker/src/components/Navbar.tsx
+++ mentaltracker/src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className="navbar-container"> {/* Add a class from styles.css */}
       <ul className="navbar-list"> {/* Add a class from styles.css */}
-        <li className="navbar-item"><Link to="/">Home</Link></li> {/* Add classes from styles.css */}
+        <li className="navbar-item"><Link to="/home">Home</Link></li> {/* Add classes from styles.css */}
         <li className="navbar-item"><Link to="/login">Login</Link></li> {/* Add classes from styles.css */}
         <li className="navbar-item"><Link to="/goals">Goals</Link></li> {/* Add classes from styles.css */}
       </ul>
@@ -16,4 +16,3 @@ const Navbar: React.FC = () => {
 };
 
 export default Navbar;
-  
